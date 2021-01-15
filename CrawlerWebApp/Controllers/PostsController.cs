18da451d4f16f85ced10b9061336891d54f0e5eb
phuonglvd00631 <phuonglvd00631@fpt.edu.vn>
2021-01-15 20:00:33 +0700

    ddd

---
+++ CrawlerWebApp/Controllers/PostsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrawlerWebApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private CrawlerDatabaseEntities db;
+
+        public PostsController()
+        {
+            db = new CrawlerDatabaseEntities();
+        }
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+           
+            return View(post);
+        }
+    }
+}
